8d3c365a0066e221745004496203e3a92863a3dc
Seledtsov Daniil <danikseledtsov@gmail.com>
2025-03-20 23:44:29 +0300

    Linked forms with models,added validation

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -17,14 +17,12 @@ def about(request):
     return render(request, 'women/about.html', { 'title': 'О сайте'})
 def addpage(request):
     if request.method == "POST":
-        form=AddPostForm(request.POST)
+        form=AddPostForm(request.POST,request.FILES)
         if form.is_valid():
             #print(form.cleaned_data)
-            try:
-                Women.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None,'Error of adding post')
+            form.save()
+            return redirect('home')
+
     else:
         form=AddPostForm()
     return render(request,'women/addpage.html',{'form':form,'title':'Добавление статьи'})
